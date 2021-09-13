c8802e477a2c504a2b8da8a6d652a196e74e9875
7DavidMedina7 <david.medina.alonzo@gmail.com>
2021-09-12 22:39:06 -0600

    Corrected the 8x12font method

--- draw_chars.c
+++ draw_chars.c
@@ -17,10 +17,10 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 8; row++) {
+  for (char row = 0; row < 12; row++) {
     unsigned short rowBits = font_8x12[c][row];
-    for (char col = 0; col < 12; col++) {
-      unsigned short colMask = 1 << (11-col);
+    for (char col = 0; col < 8; col++) {
+      unsigned short colMask = 1 << (8-col);
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
